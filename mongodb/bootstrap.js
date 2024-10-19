59baf3c4601f270d2aa89d737d0bc8d5cfc39632
Prejlin Naidu <prejlin.naidu@gmail.com>
2024-10-20 01:04:20 +0200

    mongodb init

---
+++ mongodb/bootstrap.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+const fs = require('fs');
+
+// Load schema dynamically from a JSON file
+const schemaData = JSON.parse(fs.readFileSync('./mongodb/schema.json', 'utf8'));
+const userSchema = new mongoose.Schema(schemaData);
+
+const DATABASE_URL="mongodb+srv://blessinghove69:SyZdxnA6Jgd28B6S@cluster0.s0d7k.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+
+const User = mongoose.model('User', userSchema);
+
+async function connectAndPopulate(jsonFilePath) {
+  try {
+    // Connect to MongoDB
+    await mongoose.connect(DATABASE_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log('Connected to MongoDB');
+
+    // Read and parse the data from the JSON file
+    const data = fs.readFileSync(jsonFilePath, 'utf8');
+    const users = JSON.parse(data);
+
+    // Insert data into the collection
+    await User.insertMany(users);
+    console.log('Database populated with JSON data');
+  } catch (err) {
+    console.error('Error:', err);
+  } finally {
+    mongoose.connection.close();
+  }
+}
+
+// Call the function with the JSON data file path
+connectAndPopulate('./mongodb/input.json');
